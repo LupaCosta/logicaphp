d2c76bb736d06deaa1bcb61cb95dc47e68b14c3d
Luis Paulo dos Santos Costa <VAS0036924@aluno.firjansenaisesi.com.br>
2025-02-04 19:17:44 -0300

    funcionalidade de previsao do tempo

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$nome = 'Lupa Costa';
+$idade = 20; 
+
+if ($idade >= 18)
+{
+   echo $nome .  "Você é maior de idade";} 
+else 
+{ echo  $nome . "Você é menor de idade" ;} 
+echo'<br>';
+
+
+
+
+
+echo 'hello world'; 
+echo '<br>';
+echo 'Flamengo campeão';
---
+++ previsao_tempo.php
@@ -0,0 +1,13 @@
+<?php
+
+//Crie uma lógica que a partir da temperatura me diga se o dia será ensolarado ou nublado.
+// se a temperatura for maior ou igual a 30 graus, o dis está ensolarado
+// se menor que 30 graus, o dia está nublado. 
+
+$temperatura = 33;
+
+
+
+if ($temperatura >= 30 )
+{ echo "O dia está ensolarado"; }
+else { echo 'O dia está nublado'; }
