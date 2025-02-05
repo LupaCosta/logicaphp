12f609c334c4096c60395556460c11162bfb00ee
Luis Paulo dos Santos Costa <VAS0036924@aluno.firjansenaisesi.com.br>
2025-02-05 18:34:56 -0300

    nota_escolar

--- nota_escolar.php
+++ nota_escolar.php
@@ -3,17 +3,24 @@
 //calcule a media e se o aluno teve nota maior ou igual a 7 aprovado
 //se teve nota menor que 7 reprovado
 
-$aluno = 'Lupa Costa'
-$nota_b1 = 9,2
-$nota_b2 = 8,5
-$nota_b3 = 8,9
-$nota_b4 = 6,5
+$aluno = 'Lupa Costa, ';
+$notab1 = 5;
+$notab2 = 9.5;
+$notab3 = 10;
+$notab4 = 6;
+$media = ($notab1 + $notab2 + $notab3 + $notab4) / 4;
 
-if ($nota_b1 + $nota_b2 + $nota_b3 + $nota_b4 /2 => 7,0)
+if ($media >= '7,0')
 {
-    echo $aluno . 'Está aprovado';
+    echo $aluno . 'Você Está aprovado com média ' . $media ;
 }
+
+else if ( $media < 7 && $media >= 5) 
+{
+    echo $aluno . 'Vocé está em recuperação com média,' . $media;
+}
+
 else 
 {
-    echo $aluno . 'Está reprovado';
+    echo $aluno . 'Você Está reprovado com média ' . $media ;
 }
